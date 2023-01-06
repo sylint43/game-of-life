b03df856de9a6836c4790066fb56a68c1a78e9f9
Sylint <sylvia@sylint.dev>
2023-01-05 19:58:02 -0600

    Run Life

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use std::{thread::sleep, time::Duration};
+
 use game_of_life::board::Board;
 fn main() {
-    let board = Board::random_state(20, 30);
-    println!("{}", board);
+    let mut board = Board::random_state(40, 40);
+    loop {
+        print!("\x1B[2J\x1B[1;1H"); // Clear screen, put cursor at top left
+        println!("{}", board);
+        board = board.next_board_state();
+        sleep(Duration::from_millis(100))
+    }
 }
